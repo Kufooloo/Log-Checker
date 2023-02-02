4308a49be7082e6e94b74c8c753af86b14b1b937
Jonah Wright <43395506+Kufooloo@users.noreply.github.com>
2023-02-02 22:27:09 +0000

    Added a function to return fight times

--- WipePointAnalyzer.py
+++ WipePointAnalyzer.py
@@ -5,14 +5,14 @@ from discord.commands import option
 import json
 from urllib.parse import urlparse
 
-FIGHTS = ["TOP"]
+SUPPORTED_FIGHTS = ["TOP"]
 
 class WipePoint(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.slash_command()
-    @option("fight", desciption="Fight to look for", autocomplete=discord.utils.basic_autocomplete(FIGHTS))
+    @option("fight", desciption="Fight to look for", autocomplete=discord.utils.basic_autocomplete(SUPPORTED_FIGHTS))
     async def wipepoint(self, ctx, report:String, fight):
         #check if report is valid
         o = urlparse(fight_url)
@@ -29,9 +29,32 @@ class WipePoint(commands.Cog):
         if report_id[-1] == '/':
             report_id = url_path[9:-1]
         print(report_id)
+        
+
 
         
 
 
+
+
+def returnFightStartEndTImes(report:String, encounterID:int):
+    fight_times = []
+    url = "https://www.fflogs.com/api/v2/client/"
+
+    payload = "{\"query\":\"query report($code:String!){\\n\\treportData{\\n\\t\\treport(code:$code){\\n\\t\\t\\tfights{\\n\\t\\t\\t\\tstartTime\\n\\t\\t\\t\\tendTime\\n\\t\\t\\t\\tencounterID\\n\\t\\t\\t}\\n\\t\\t}\\n\\t}\\n}\",\"operationName\":\"report\",\"variables\":{\"code\":\"" + report + "\"}}"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + bearer_token 
+    }
+
+    response = requests.request("POST", url, data=payload, headers=headers)
+    contents = response.json()
+    fightList = contents['data']['reportData']['report']['fights']
+    for fight in fightList:
+        if fight.get("encounterID") == encounterID:
+            temp = (fight.get("startTime"), fight.get("endTime"))
+            fight_times.append(temp)
+    
+    return fight_times
 
 
