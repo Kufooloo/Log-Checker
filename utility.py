67cbae1dbd5b8f1fac3b8c04e83ab3cc8cac17ca
Jonah Wright <robot2boy@icloud.com>
2023-04-02 20:19:12 -0700

    Added utility

---
+++ utility.py
@@ -0,0 +1,63 @@
+import requests
+import discord
+from discord.ext import tasks, commands
+from discord.commands import option 
+import datetime
+from urllib.parse import urlparse
+import asyncio
+
+class utility(commands.Cog):
+    def __init__(self, bot) -> None:
+        super().__init__()
+        self.bot = bot
+        self.response = []
+    @commands.Cog.listener()
+    async def on_message(self, ctx):
+        content = ctx.content
+        print(content)
+        token_list = token(content)
+        print(token_list)
+        i = 0
+        test =True
+        url = ""
+        while i < len(token_list) and test == True:
+            o = urlparse(token_list[i])
+            print(o)
+            if o.netloc == 'twitter.com':
+                print('found twitter link')
+                url = o._replace(netloc="www.vxtwitter.com").geturl()
+                test = False
+            i+=1
+        
+        if test == True:
+            return
+        
+        self.reponse = [url,ctx]
+        await ctx.add_reaction('📹')
+        await asyncio.sleep(10)
+        await ctx.remove_reaction('📹')
+        self.response = []
+        return
+    @commands.Cog.listener()
+    async def on_reaction_add(self, reaction, user):
+        if user.id == self.response[1].id and reaction.emoji == "📹":
+            self.reponse[1].send(self.reponse[0])
+        return
+
+        
+        
+
+
+                
+
+def token(string):
+    start, i = 0, 0
+    token_list = []
+    for x in range(0, len(string)):
+        if " " == string[i:i+1]:
+            token_list.append(string[start:i+1])
+            start = i + 1
+        i += 1
+    token_list.append(string[start:i+1])
+    return token_list
+    
